c2550f17d5e29286c81efb071743621d723f65d6
YanakornS <155708787+YanakornS@users.noreply.github.com>
2024-06-23 19:42:39 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import Search from "./Search";
+import Restaurant from "./Restaurant";
+import Card from "./Card";
+import Header from "./Header";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container flex flex-col items-center m-auto space-y-6">
+        <Header />
+        <Search />
+        <Restaurant />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const Card = ({ img, title, type }) => (
+  <div>
+    <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={img} // ใช้ prop img แทน URL ที่เขียนตรงนี้
+          alt={title} // ใช้ prop title เป็น alt text
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{type}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
